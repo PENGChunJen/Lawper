5cb8fb713c98d0560c53995bcad83201cf003b9e
PENGChunJen <b00901081@ntu.edu.tw>
2016-06-18 02:48:17 +0800

    find similar uncut sentences and similar jeiba_cut sentences

--- Crawler/jeiba_cut_similar_sentence.py
+++ Crawler/jeiba_cut_similar_sentence.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import gensim
 import re
+import jieba
 model = gensim.models.Word2Vec.load("cut_words.model")
 X = model.syn0
 
@@ -12,17 +13,17 @@ def print_similar_words(keyword):
     print ''
 
 def find_sentence(keyword):
-    p = keyword+ur'(.*?)。'
+    p = ' '+keyword+ur' (.*?)。'
     pattern = re.compile(p, re.UNICODE)
     sentences = re.findall(pattern, text)
     return sentences
 
 def print_similar_sentence(keyword):
     sentences = find_sentence(keyword)
-    if sentences: 
+    if sentences:
         print "keyword:", keyword
         for s in sentences:
-            print '\t'+keyword.encode('utf8')+s.encode('utf8')+'。\n'
+            print '\t'+keyword.encode('utf8')+s.replace(' ','').encode('utf8')+'。\n'
 
 def print_all_similar_sentences(keyword):
     print_similar_sentence(keyword)
@@ -34,6 +35,9 @@ def print_all_similar_sentences(keyword):
 
 text = open('../data/crash_JAN/2015-01-08_TYD_M_3', 'rb').read()
 text = text.replace('　', '').replace(' ', '').replace('\r\n', '').decode('utf8', 'ignore').encode('raw_unicode_escape').replace('\u3000','').decode('raw_unicode_escape')
+seg_list = jieba.cut(text, cut_all=False)
+text = " ".join(seg_list)
+
 
 keyword = u"查"
 print_all_similar_sentences(keyword)
